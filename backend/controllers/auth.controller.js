0aba1f668204975ca9e56fc55e3fb13a085cbae7
alexandre <alexandre020602@gmail.com>
2026-05-09 22:55:18 -0300

    feat: implement user signup functionality with validation and password hashing

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,10 +1,42 @@
+import { User } from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+
+
 export const signup = async (req, res) => {
-    res.send('signup route');
+   const { name, email, password } =  req.body; 
+   try {
+    if(!name || !email || !password) {
+        throw new Error('All fields are required');
+    }
+
+    const userAlreaduExists = await User.findOne({ email });
+    if(userAlreaduExists) {
+        return res.status(400).json({succes: false, message: "User already exists"});
+    }
+
+    const hashedPassword = await bcrypt.hashSync(password, 10);
+    const user = await new User({
+        name,
+        email,
+        password: hashedPassword
+    });
+
+    await user.save();
+    res.status(201).json({ success: true, message: "User created successfully" });
+
+
+   } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+    console.log(error);
+   }
 }
+
+
 
 export const login = async (req, res) => {
     res.send('login route');
 }
+
 
 export const logout = async (req, res) => {
     res.send('logout route');
