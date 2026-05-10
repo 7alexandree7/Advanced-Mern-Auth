bf0bcdc315d5c9f391d11cea5266d617d45ec711
alexandre <alexandre020602@gmail.com>
2026-05-10 12:50:02 -0300

    feat: enhance user signup process with verification token and update README

--- backend/config/env.js
+++ backend/config/env.js
@@ -3,12 +3,22 @@ import 'dotenv/config';
 export const ENV_VARIABLES = {
     MONGODB_URL: process.env.MONGODB_URL,
     PORT: process.env.PORT || 3000,
+    JWT_SECRET: process.env.JWT_SECRET,
+    NODE_ENV: process.env.NODE_ENV || 'development',
 };
 
 if (!ENV_VARIABLES.MONGODB_URL) {
     throw new Error('MONGODB_URL is not defined');
 }
 
+if (!ENV_VARIABLES.JWT_SECRET) {
+    throw new Error('JWT_SECRET is not defined');
+}
+
 if (!ENV_VARIABLES.PORT) {
     throw new Error('PORT is not defined');
+}
+
+if (!ENV_VARIABLES.NODE_ENV) {
+    throw new Error('NODE_ENV is not defined');
 }
--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { User } from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import { generateVerificationCode } from '../utils/generateverificationToken.js';
 
 
 export const signup = async (req, res) => {
@@ -15,13 +16,18 @@ export const signup = async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hashSync(password, 10);
+    const verificationToken = generateVerificationToken();
+
     const user = await new User({
         name,
         email,
-        password: hashedPassword
+        password: hashedPassword,
+        verificationToken,
+        verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, // Set token to expire in 24 hours
     });
 
     await user.save();
+    generatetokenAndSetCookie(res, user._id);
     res.status(201).json({ success: true, message: "User created successfully" });
 
 
